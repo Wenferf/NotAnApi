c13d5d7ecab73d87bfbd0c5242775c0197cb81a7
Tucker Gillbrand <tucker@gillbrand.com>
2021-08-03 23:26:58 -0700

    Setting up initial lists

--- NotAnApi/ContentView.swift
+++ NotAnApi/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var things = ["First Thing", "Second Thing", "Third Thing", "Fourth Thing", "Fifth Thing"]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                
+                ForEach(things, id: \.self) { thing in
+                    Text(thing)
+                }
+                .onMove(perform: { indices, newOffset in
+                    things.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                .onDelete(perform: { indexSet in
+                    things.remove(atOffsets: indexSet)
+                })
+                
+            }
+            .navigationBarTitle("Things", displayMode: .inline)
+            .navigationBarItems(leading: EditButton())
+        }
     }
 }
 
@@ -18,4 +34,13 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct ToDoItem: Identifiable {
+    var id = UUID()
+    var label = String()
+    var calorie = Int()
+    var fat = Float()
+    var protien = Float()
+    
 }
