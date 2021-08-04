37bc47a789ba94f596560ecdf32439daa32933bf
Tucker Gillbrand <tucker@gillbrand.com>
2021-08-03 23:24:07 -0700

    Initial Commit

---
+++ NotAnApi/NotAnApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotAnApiApp.swift
+//  NotAnApi
+//
+//  Created by Tucker(School) on 8/3/21.
+//
+
+import SwiftUI
+
+@main
+struct NotAnApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
